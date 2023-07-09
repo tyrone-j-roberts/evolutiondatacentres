f6e9a62712b6c7e084ca9c2867b5109379d8ed40
Tyrone Roberts <hi@tyronejroberts.co.uk>
2023-07-09 13:12:56 +0100

    Wicked

--- acf-blocks/markets-map-locations.php
+++ acf-blocks/markets-map-locations.php
@@ -31,7 +31,7 @@ include(locate_template('/acf-blocks/common/block_header.php')); ?>
                 <div class="location-content__text">
                     <h3>Philippines</h3>
                     <p>
-                        <strong>Population dynamics</strong><br/>
+                        <strong>Population Dynamics</strong><br/>
                         The Philippines has a young population who are heavy users of technology. 
                         Their average daily use of social media is over four hours which is 60% higher than the rest of the world. 
                         82% of Filipinos have social media accounts, with a median age of 26.3 and 1.5% population growth annually. 
@@ -39,27 +39,34 @@ include(locate_template('/acf-blocks/common/block_header.php')); ?>
                     </p>
                     <p>&nbsp;</p>
                     <p>
-                        <strong>Renewable energy</strong><br/>
+                        <strong>Renewable Energy</strong><br/>
                         The Philippines’ energy market is liberalised with generation, distribution, and sales open to private participation, which means users are able to contract directly with renewable energy developers. 
                         Although its energy mix is dominated by coal (47%) and natural gas (22%), it has the 3rd largest geothermal resource in the world, which makes it a very good option for baseload demand. 
                         <br/><br/>
                         Together with a moratorium on new coal plants, there is a national target to increase the share of renewable energy in the primary mix to 50% by 2040, this includes the addition of new renewable capacity of 9.9GW by 2030. These factors are driving the largest renewable energy development in South East Asia.
                     </p>
                     <div class="graph">
-                        <strong>Renewable energy mix</strong>
+                        <strong>Renewable Energy Mix</strong>
                         <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/philippines-chart.png" alt="Renewable Energy Mix" />
                     </div>
                     <p>
-                        <strong>Market regulations</strong><br />
+                        <strong>Market Regulations</strong><br />
                         The electricity market is partially deregulated, with a growing demand for corporate PPAs. 
                         There is a favourable regulatory climate as a result of laws like the revisions to the 
                         Foreign Investment Act, the Public Service Act, and the Retail Trade Liberalization Act.
                     </p>
-                    <p>
-                        <strong>Site overview</strong><br />
-                        The site is located in Gateway Business Park, General Trias, Cavite, the Philippines and occupies an area of around 40,000 sqm. We are working with Megawide Construction Corp, who is a conglomerate with substantial infrastructure and renewable power capabilities in the region. 
-                        The IT load is able to support up to 69MW, split into 3 stages with a projected PUE of &lt;1.4.
+                    <p style="margin-bottom: 0.5em">
+                        <strong>Market Drivers</strong><br />
+                        The following are the factors driving the demand for data centres:
+                 
                     </p>
+                    <ul>
+                            <li>Business Process Outsourcing growth</li>
+                            <li>Enterprise Cloud adoption</li>
+                            <li>5G Network expansion</li>
+                            <li>Growth of AI and Machine Learning</li>
+                    </ul>
+                    <button style="margin-top: 1em" type="button" class="button-link" data-modal="LocationOverviewModal_Philippines">View more</button>
                 </div>
                 <div class="location-content__media">
 
@@ -73,7 +80,7 @@ include(locate_template('/acf-blocks/common/block_header.php')); ?>
                             <?php for ($i = 1; $i <= 3; $i++): ?>
                             <div class="image-slide">
                                 <div class="image">
-                                    <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/philippines-slide-image-<?= $i ?>.jpg" alt="Philippines Image <?= $i ?>" />
+                                    <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/philippines-slide-image-<?= $i ?>.jpg?version=1" alt="Philippines Image <?= $i ?>" />
                                 </div>
                             </div>
                             <?php endfor; ?>
@@ -93,24 +100,24 @@ include(locate_template('/acf-blocks/common/block_header.php')); ?>
                 <div class="location-content__text">
                     <h3>Thailand</h3>
                     <p>
-                        <strong>Population dynamics</strong><br/>
+                        <strong>Population Dynamics</strong><br/>
                         With a GDP of 506 billion USD, Thailand is a dynamic market in Southeast Asia. 
                         The population is very technology focused, with 85% using the internet and averaging over 2.5 hours per day on social media.
                     </p>
                     <p>&nbsp;</p>
                     <p>
-                        <strong>Renewable energy</strong><br/>
+                        <strong>Renewable Energy</strong><br/>
                         Thailand’s power market is partially liberalised. Generation is open to private companies while distribution, transmission and sales are operated by state owned utilities. The country’s energy mix is dominated by natural gas 66% and coal 20%, biofuels and hydro account for 9% and 3% respectively while solar makes up ~3%.
                         <br/><br/>
                         The government has set a target of 30% renewable energy for power capacity in 2036; there are also targets of 15 GW (now at 3.4GW) of solar  and 3GW (now 1.5GW) of wind; 
                         They also aim to be net zero by 2065 but have not set tangible renewable energy targets.
                     </p>
                     <div class="graph">
-                        <strong>Renewable energy mix</strong>
+                        <strong>Renewable Energy Mix</strong>
                         <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/thailand-chart.png" alt="Renewable Energy Mix" />
                     </div>
                     <p>
-                        <strong>Market regulations</strong><br />
+                        <strong>Market Regulations</strong><br />
                         Thailand imposes stringent requirements on the construction of data centres. 
                         A business planning on building and operating a data centre in Thailand must obtain a 
                         BOI Investment Promotion Certificate and a Telecom License. At 2022 APEC Summit 
@@ -118,10 +125,17 @@ include(locate_template('/acf-blocks/common/block_header.php')); ?>
                         and sustainability, prioritising recovery and growth, while keeping an eye on sustainable 
                         development that will allow the country to thrive.
                     </p>
-                    <p>
-                        <strong>Site overview</strong><br />
-                        The site will be located at Bangan Trad Road, Bangkok and occupies an area of around 20,000 sqm. We are working with one of the largest property and development companies in Thailand. The IT load is able to support up to 34MW, split into 2 stages with a projected PUE of &lt;1.4.
+                    <p style="margin-bottom: 0.5em">
+                        <strong>Market Drivers</strong><br />
+                        The following are the factors driving the demand for data centres:
                     </p>
+                    <ul>
+                        <li>Enterprise Cloud adoption</li>
+                        <li>Adoption of AI and Machine Learning</li>
+                        <li>5G Network expansion</li>
+                        <li>eCommerce and Digital Services growth</li>
+                    </ul>
+                    <button style="margin-top: 1em" type="button" class="button-link" data-modal="LocationOverviewModal_Thailand">View more</button>
                 </div>
                 <div class="location-content__media">
 
@@ -154,7 +168,7 @@ include(locate_template('/acf-blocks/common/block_header.php')); ?>
                 <div class="location-content__text">
                     <h3>Vietnam</h3>
                     <p>
-                        <strong>Population dynamics</strong><br/>
+                        <strong>Population Dynamics</strong><br/>
                         Vietnam has a relatively young population with a median age of 32. 71% of 
                         the population uses the internet, with an average of 2.5 hours per day spent on social media. 
                         The country is one of the fastest-growing digital economies in 
@@ -163,21 +177,33 @@ include(locate_template('/acf-blocks/common/block_header.php')); ?>
                     </p>
                     <p>&nbsp;</p>
                     <p>
-                        <strong>Renewable energy</strong><br/>
+                        <strong>Renewable Energy</strong><br/>
                         Vietnam’s energy market is heavily regulated with transmission, distribution and sales controlled by the state-owned Vietnam Electricity (EVN), although generation is partially liberalised. The energy mix is comprised largely of coal 53% and hydro 26%; followed by natural gas 16% and other renewables 5%; although Vietnam already accounts for the largest solar capacity in Southeast Asia.
                         <br/><br/>
                         The country has a draft Power Development Plan (Draft PDP8) targeting an increase in renewable generation to 32% by 2030, outstripping even coal which is expected to account for 27%.
                     </p>
                     <div class="graph">
-                        <strong>Renewable energy mix</strong>
+                        <strong>Renewable Energy Mix</strong>
                         <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/vietnam-chart.png" alt="Renewable Energy Mix" />
                     </div>
                     <p>
-                        <strong>Market regulations</strong><br />
+                        <strong>Market Regulations</strong><br />
                         Vietnam is seeking to reach carbon neutrality by 2050. The country has a favourable regulatory landscape for data centre investment, including a decision in 2020 to approve a National Digital Transformation Programme, a cybersecurity law, and a decree to force onshore online service providers to store Vietnamese users’ information inside the country.
                         <br/><br/>
                         The Power Development Master Plan 8 in Vietnam represents the latest market regulations in the country's power sector. It outlines the strategic roadmap for power generation, transmission, and distribution, with a focus on renewable energy development, grid integration, and enhancing energy efficiency to meet the growing electricity demand and sustainability goals.
                     </p>
+                    <p style="margin-bottom: 0.5em">
+                        <strong>Market Drivers</strong><br />
+                        The following are the factors driving the demand for data centres:
+                    </p>
+                    <ul>
+                        <li>Digital Economy Growth</li>
+                        <li>Internet and Mobile Penetration</li>
+                        <li>Enterprise Cloud Adoption</li>
+                        <li>5G Network Expansion</li>
+                        <li>AI and Big Data Analytics</li>
+                    </ul>
+                    <button style="margin-top: 1em" type="button" class="button-link" data-modal="LocationOverviewModal_Vietnam">View more</button>
                 </div>
                 <div class="location-content__media">
                     <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/vietnam-location.svg?t=<?= time() ?>" 
@@ -195,25 +221,35 @@ include(locate_template('/acf-blocks/common/block_header.php')); ?>
                 <div class="location-content__text">
                     <h3>Indonesia</h3>
                     <p>
-                        <strong>Population dynamics</strong><br/>
+                        <strong>Population Dynamics</strong><br/>
                         Indonesia has more than 210 million internet users out of a total population of 270 million, and daily social media usage is high at an average of 3.28 hours per day. It also has the highest GDP in Southeast Asia at 1.186 trillion USD, making it an important data centre investment market.
                     </p>
                     <p>&nbsp;</p>
                     <p>
-                        <strong>Renewable energy</strong><br/>
+                        <strong>Renewable Energy</strong><br/>
                         There has been a clear transition in Indonesia away from coal to green power. Clean energy currently makes up 18% of the country’s production, with hydro 8%, bioenergy 5% and geothermal 5% being the leading forms of alternative energy generation.<br/>
                         The market is partly liberalised; private companies can participate in generation while distribution, transmission, and sales are controlled by  the state owned company PLN.<br/>
                         <br/><br/>
                         The government has set a target of 23% renewable energy by 2025 and 31% by 2050. They have set out a 10-year electricity procurement plan (RUPTL.2) where the government targets a 52% share of renewables in new capacity additions (21GW out of total 41GW, mainly from hydro, solar and geothermal).
                     </p>
                     <div class="graph">
-                        <strong>Renewable energy mix</strong>
+                        <strong>Renewable Energy Mix</strong>
                         <img src="<?= get_stylesheet_directory_uri() ?>/assets/images/indonesia-chart.png" alt="Renewable Energy Mix" />
                     </div>
                     <p>
-                        <strong>Market regulations</strong><br />
+                        <strong>Market Regulations</strong><br />
                         The Indonesian government mandates that new data centres must be built and operated using locally-sourced materials and power. In addition data centres serving the financial sector must abide by the banking rules and meet IT compliance standards.
                     </p>
+                    <p style="margin-bottom: 0.5em">
+                        <strong>Market Drivers</strong><br />
+                        The following are the factors driving the demand for data centres:
+                    </p>
+                    <ul>
+                        <li>Enterprise Cloud Adoption</li>
+                        <li>5G Network expansion</li>
+                        <li>Internet and Mobile user growth</li>
+                        <li>AI and Big Data Analytics</li>
+                    </ul>
                 </div>
                 <div class="location-content__media">
                     <div style="width: 100%; margin: 0 auto; display: block;">
--- template-parts/map-locations-overview.php
+++ template-parts/map-locations-overview.php
@@ -1,13 +1,13 @@
 <?php $icon_file_path = get_stylesheet_directory() . '/assets/images'; ?>
 <div class="location-overview" id="LocationOverview_Philippines">
     <div class="location-overview__stats">
-        <h4>Manila</h4>
+        <h4>Manila, Philippines</h4>
         <div class="location-overview__stat cap">
             <div class="icon-wrapper">
                 <?= file_get_contents("{$icon_file_path}/icon-data-center.svg"); ?>
             </div>
             <div class="data">
-                +60MW
+                Capacity (IT Load): 69MW
             </div>
         </div>
         <div class="location-overview__stat year">
@@ -15,31 +15,29 @@
                 <?= file_get_contents("{$icon_file_path}/icon-date.svg"); ?>
             </div>
             <div class="data">
-                2024
+                COD: 2024
             </div>
         </div>
     </div>
     <div class="location-overview__details">
-        <h4>Philippines</h4>
         <ul>
-            <li>Business Process Outsourcing growth</li>
-            <li>Enterprise Cloud adoption</li>
-            <li>5G Network expansion</li>
-            <li>Growth of AI and Machine Learning</li>
+            <li>Location: Gateway Business Park, General Trias, Cavite</li>
+            <li>Size: 40,000 sqm</li>
+            <li>Partner: Megawide Construction Corp.</li>
+            <li>Projected PUE: &lt;1.4</li>
         </ul>
-        <button type="button" class="button-link" data-modal="LocationOverviewModal_Philippines">View more</button>
     </div>
 </div>
 
 <div class="location-overview" id="LocationOverview_Thailand">
     <div class="location-overview__stats">
-        <h4>Bangkok</h4>
+        <h4>Bangkok, Thailand</h4>
         <div class="location-overview__stat cap">
             <div class="icon-wrapper">
                 <?= file_get_contents("{$icon_file_path}/icon-data-center.svg"); ?>
             </div>
             <div class="data">
-                +30MW
+                Capacity (IT Load): 34MW
             </div>
         </div>
         <div class="location-overview__stat year">
@@ -47,31 +45,29 @@
                 <?= file_get_contents("{$icon_file_path}/icon-date.svg"); ?>
             </div>
             <div class="data">
-                2024
+                COD: 2024
             </div>
         </div>
     </div>
     <div class="location-overview__details">
-        <h4>Thailand</h4>
         <ul>
-            <li>Enterprise Cloud adoption</li>
-            <li>Adoption of AI and Machine Learning</li>
-            <li>5G Network expansion</li>
-            <li>eCommerce and Digital Services growth</li>
+            <li>Location: Bang NA Trad Road, Bangkok, Thailand</li>
+            <li>Size: 23,000 sqm</li>
+            <li>Partner: Central Pattana PCL</li>
+            <li>Projected PUE: &lt;1.4</li>
         </ul>
-        <button type="button" class="button-link" data-modal="LocationOverviewModal_Thailand">View more</button>
     </div>
 </div>
 
 <div class="location-overview" id="LocationOverview_Vietnam">
     <div class="location-overview__stats">
-        <h4>HCMC</h4>
+        <h4>HCMC, Vietnam</h4>
         <div class="location-overview__stat cap">
             <div class="icon-wrapper">
                 <?= file_get_contents("{$icon_file_path}/icon-data-center.svg"); ?>
             </div>
             <div class="data">
-                +20MW
+                Capacity (IT Load): 20MW
             </div>
         </div>
         <div class="location-overview__stat year">
@@ -79,50 +75,18 @@
                 <?= file_get_contents("{$icon_file_path}/icon-date.svg"); ?>
             </div>
             <div class="data">
-                2025
+            >   COD: 2024
             </div>
         </div>
     </div>
     <div class="location-overview__details">
-        <h4>Vietnam</h4>
-        <ul>
-            <li>Digital Economy Growth</li>
-            <li>Internet and Mobile Penetration</li>
-            <li>Enterprise Cloud Adoption</li>
-            <li>5G Network Expansion</li>
-            <li>AI and Big Data Analytics</li>
-        </ul>
-        <button type="button" class="button-link" data-modal="LocationOverviewModal_Vietnam">View more</button>
     </div>
 </div>
 
 <div class="location-overview" id="LocationOverview_Indonesia">
     <div class="location-overview__stats" style="border: 2px solid transparent;">
-        <h4>&nbsp;</h4>
-        <div class="location-overview__stat cap">
-            <div class="icon-wrapper">
-                &nbsp;
-            </div>
-            <div class="data">
-                &nbsp;
-            </div>
-        </div>
-        <div class="location-overview__stat year">
-            <div class="icon-wrapper">
-                &nbsp;
-            </div>
-            <div class="data">
-                &nbsp;
-            </div>
-        </div>
+        <h4>Indonesia</h4>
     </div>
     <div class="location-overview__details">
-        <h4>Indonesia</h4>
-        <ul>
-            <li>Enterprise Cloud Adoption</li>
-            <li>5G Network expansion</li>
-            <li>Internet and Mobile user growth</li>
-            <li>AI and Big Data Analytics</li>
-        </ul>
     </div>
 </div>
